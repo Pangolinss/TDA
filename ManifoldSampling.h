20cc8071155feac90f744c1e0fbb0578ba1d87c0
unknown <derekwu114@gmail.com>
2022-12-28 17:35:41 -0500

    Implemented stereographic normal dist and started using Eigen for the linear algebra

--- ManifoldSampling.h
+++ ManifoldSampling.h
@@ -1,58 +1,88 @@
 #ifndef MANIFOLDSAMPLING_H
 #define MANIFOLDSAMPLING_H
 #include<random>
+#include<iostream>
 #include<chrono>
-#include <math.h>
+#include<math.h>
+#include<functional>
+#include<Eigen/Core>
+#include<Eigen/Eigenvalues>
+#include<Eigen/Cholesky>
 
-template<typename RealType>
+template<typename RealType, int Emb_Dim>
 class Sampler{
-    int emb_dim;
     public:
-    Sampler(int d) : emb_dim{d} {}
-    virtual RealType* UniformSample(RealType* arr, int num)=0; //samples num points from the manifold, if arr is nullptr will allocate an array
+    static const int emb_dim = Emb_Dim;
+    template<int n>
+    using EmbMatrix = Eigen::Matrix<RealType, Emb_Dim, n>;
+    using EmbVector = Eigen::Matrix<RealType, Emb_Dim, 1>;
+    Sampler(){}
+    virtual void UniformSample(EmbMatrix<-1>& samples, int num)=0; //samples num points from the manifold, if arr is nullptr will allocate an array
                                                           //Consecutive elements are components of the coordiantes
-    int embedded_dim() const {return emb_dim;}
+    const int embedded_dim() const {return emb_dim;}
 };
 
 template<int Dim, typename RealType>
-class CircleSampler : public Sampler<RealType>{
+class CircleSampler : public Sampler<RealType, Dim+1>{
     RealType r;
     public:
-    CircleSampler() : Sampler<RealType>{Dim+1}, r{1} {}
-    CircleSampler(RealType r_) : Sampler<RealType>{Dim+1}, r{r_} {}
-    RealType* UniformSample(RealType* arr, int num) override{
+    template<int n>
+    using CoordMatrix = Eigen::Matrix<RealType, Dim, n>;
+    using CoordSquareMatrix = Eigen::Matrix<RealType, Dim, Dim>;
+    using CoordVector = Eigen::Matrix<RealType, Dim, 1>;
+    template<int n>
+    using EmbMatrix = Eigen::Matrix<RealType, Sampler<RealType, Dim+1>::emb_dim, n>;
+    using EmbVector = Eigen::Matrix<RealType, Sampler<RealType, Dim+1>::emb_dim, 1>;
+    CircleSampler() : Sampler<RealType, Dim+1>{}, r{1} {}
+    CircleSampler(RealType r_) : Sampler<RealType, Dim+1>{}, r{r_} {}
+    void UniformSample(EmbMatrix<-1>& samples, int num) override{
         std::uniform_real_distribution<RealType> dist1(0, M_PI);
         std::uniform_real_distribution<RealType> dist2(0, 2*M_PI);
         unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
         std::default_random_engine generator (seed);
-        RealType* ret = nullptr;
-        if (arr == nullptr){
-            arr = new RealType[this->embedded_dim() * num];
-            ret = arr;
-        }
         int j;
         for (int i = 0; i < num; ++i){
             for (j = 0; j < Dim; ++j){
-                arr[i*this->embedded_dim() + j] = dist1(generator);
+                samples(j,i) = dist1(generator);
             }
-            arr[i*this->embedded_dim() + j-1] = dist2(generator);
+            samples(j-1,i) = dist2(generator);
             RealType acc = 1.0;
             for (j = 0; j < Dim; ++j){
-                RealType temp = acc * cos(arr[i*this->embedded_dim() + j]);
-                acc *= sin(arr[i*this->embedded_dim() + j]);
-                arr[i*this->embedded_dim() + j] = temp;
+                RealType temp = acc * cos(samples(j,i));
+                acc *= sin(samples(j,i));
+                samples(j,i) = temp;
             }
-            arr[i*this->embedded_dim() + j] = acc;
+            samples(j,i) = acc;
         }
-        if (ret != nullptr) return ret;
-        else return nullptr;
     }
-    RealType* NormalSample(RealType* arr, int num, RealType* mu = nullptr, RealType sd = 1.0){
-        std::default_random_engine generator;
-        std::normal_distribution<RealType> distribution(mu,sd);
-        
+    //Must be a valid covariance matrix
+    void StereographicNormalSample(EmbMatrix<-1>& samples, const CoordVector& mu, const CoordSquareMatrix& cov){
+        //Using mean and covariance matrix generate normal distribution in R^n
+        int num = samples.cols();
+        Eigen::LLT<CoordSquareMatrix> llt(cov);
+        auto L = llt.matrixL();
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        std::default_random_engine generator(seed);
+        std::normal_distribution<RealType> distribution(0,1);
+        RealType* ret = nullptr;
+        Eigen::Matrix<RealType, -1, -1> data(Dim, num);
+        for (auto& it : data.reshaped()){
+            it = distribution(generator);
+        }
+        Eigen::Matrix<RealType, -1, -1> res = L * data;
+        res.colwise() += mu;
+        //Use a stereographic projection to send our points in R^n -> R^n+1 on the unit circle
+        int i = 0;
+        for (auto vec : samples.colwise()){
+            RealType s_sq = res.col(i).squaredNorm();
+            vec[vec.size()-1] = (s_sq-1)/(s_sq+1);
+            for (int j = 0; j < vec.size()-1; ++j){
+                vec[j] = 2 * res.col(i)[j]/(s_sq + 1);
+            }
+            i++;
+        }
+    }
 
-    }
 };
 
 #endif
--- main.cc
+++ main.cc
@@ -1,28 +1,34 @@
 #include<iostream>
 #include"Simplex.h"
 #include"ManifoldSampling.h"
-#define DIM 1
+#define DIM 4
 
 int main(){
-    Simplex simp(100);
-    int word[] = {1,2,3,4,5};
-    int word2[] = {1,2,3,7,8, 9};
-    int word3[] = {10,11,12};
-    simp.insert(word, 5);
-    simp.insert(word2, 6);
-    simp.insert(word3, 3);
+    // Simplex simp(100);
+    // int word[] = {1,2,3,4,5};
+    // int word2[] = {1,2,3,7,8, 9};
+    // int word3[] = {10,11,12};
+    // simp.insert(word, 5);
+    // simp.insert(word2, 6);
+    // simp.insert(word3, 3);
     CircleSampler<DIM, double> cs{};
     int num_samples = 10;
-    double* r = cs.UniformSample(nullptr, num_samples);
+    CircleSampler<DIM, double>::EmbMatrix<-1> mat(DIM+1, 10);
+    cs.UniformSample(mat, num_samples);
     double acc = 0;
-    for (int i = 0; i < num_samples; ++i){
-        acc = 0;
-        for (int j = 0; j < DIM+1; ++j){
-            std::cout<<r[i*(DIM+1) + j]<<" ";
-            acc += r[i*(DIM+1)+j] * r[i*(DIM+1)+j];
-        }
-        std::cout<<" | "<<acc;
-        std::cout<<std::endl;
-    }
+    std::cout<<mat;
+    std::cout<<mat.transpose() * mat;
+    // CircleSampler<DIM, double>::CoordVector vec = CircleSampler<DIM, double>::CoordVector::Zero();
+    // CircleSampler<DIM, double>::CoordSquareMatrix cov;
+    // int i = 0;
+    // for (auto& it : cov.reshaped()){
+    //     it = i/5.0;
+    //     i++;
+    // }
+    // cov = cov*cov.transpose();
+    // CircleSampler<DIM, double>::EmbMatrix<-1> mat(DIM+1, 10);
+    // cs.StereographicNormalSample(mat, vec, cov);
+    // std::cout<<mat;
+    // std::cout<<mat.transpose()* mat;
     return 0;
 }
